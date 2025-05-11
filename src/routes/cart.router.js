2f8d68c46ff4fa50dc87108083c610f0d4c4982f
Maia Fazzini <maiafazzini@gmail.com>
2025-05-11 13:03:20 -0300

    logica del servidor con mongo DB sin handlebars ok

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -2,23 +2,28 @@ import express from "express"
 import {addProductInCartById, createCart, getCartById, deleteProductInCartById,deleteAllProductsInCart,updateAllProductsInCart, updateQuantityOfProductInCartById } from "../controllers/carts.controllers.js"
 
 const cartRouter = express.Router();
-/* o	PUT api/carts/:cid/products/:pid deberá poder actualizar SÓLO la cantidad de ejemplares del producto por cualquier cantidad pasada desde req.body */
+
 
 //? Manejo de Carritos (/api/carts/) 
 //*nuevos
+
 //deberá eliminar del carrito el producto seleccionado.
 cartRouter.delete("/:cid/products/:pid", deleteProductInCartById)
-
 //deberá eliminar todos los productos del carrito 
 cartRouter.delete("/:cid", deleteAllProductsInCart)
 //deberá actualizar todos los productos del carrito con un arreglo de productos.
 cartRouter.put("/:cid", updateAllProductsInCart)
-
 //deberá poder actualizar SÓLO la cantidad de ejemplares del producto por cualquier cantidad pasada desde req.body en un objeto
 cartRouter.put("/:cid/products/:pid", updateQuantityOfProductInCartById)
 
 //* anteriores
+
+//trae el carrito por su id y mustra un detalle completo del producto
 cartRouter.get("/:cid", getCartById)
+//crea un nuevo carrito vacio
 cartRouter.post("/", createCart)
+//agrega producto al carrito y si el producto existe suma las cantidades
 cartRouter.post("/:cid/product/:pid", addProductInCartById)
+
+
 export default cartRouter
